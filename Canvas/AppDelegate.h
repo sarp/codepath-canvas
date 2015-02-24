f37836567256b8acf86acac97df0df7ba6cd338c
Sarp Centel <sarp@loaner-C02KJ061FJ47.local>
2015-02-24 09:57:59 -0500

    Initial Commit

---
+++ Canvas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Canvas
+//
+//  Created by Sarp Centel on 2/24/15.
+//  Copyright (c) 2015 Sarp Centel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
